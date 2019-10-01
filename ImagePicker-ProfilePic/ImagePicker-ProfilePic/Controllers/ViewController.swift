4e3b415b39b4ce3814ff032f1c2bb5c8f9ad80f9
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-01 14:37:20 -0400

    initial commit

---
+++ ImagePicker-ProfilePic/ImagePicker-ProfilePic/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePicker-ProfilePic
+//
+//  Created by Sunni Tang on 10/1/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
